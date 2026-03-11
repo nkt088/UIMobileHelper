95833651645dd4b2af575dbad6c40a7cbc941f79
Nikita Makhov <l0ginow.nick@yandex.ru>
2026-03-11 20:42:16 +0300

    Initial commit

---
+++ UIMobileHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UIMobileHelper
+//
+//  Created by Nikita Makhov on 11.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UIMobileHelper/UIMobileHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIMobileHelperApp.swift
+//  UIMobileHelper
+//
+//  Created by Nikita Makhov on 11.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct UIMobileHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
